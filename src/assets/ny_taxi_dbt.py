6e4362f54fe5c84d6bb6f16c890fbd99cc18e2b2
AIZharau <alieszhar@gmail.com>
2025-02-21 22:14:42 +0300

    fix path in asset

--- src/assets/ny_taxi_dbt.py
+++ src/assets/ny_taxi_dbt.py
@@ -1,40 +1,24 @@
 import dagster as dg
 from dagster_dbt import dbt_assets, DbtCliResource, DbtProject
 from pathlib import Path
-import subprocess
-import tempfile
-import shutil
 
 
-# Clone repository to temporary directory
-with tempfile.TemporaryDirectory() as tmp_dir:
-    repo_url = "https://github.com/AIZharau/de-zoomcamp-2025.git"
-    subprocess.run(
-        ["git", "clone", "--depth", "1", repo_url, tmp_dir],
-        check=True
-    )
-    
-    # Path to dbt project inside cloned repository
-    dbt_project_directory = Path(tmp_dir) / "dbt_clickhouse_taxi_rides"
-    
-    # Install dbt dependencies
-    subprocess.run(["dbt", "deps"], cwd=dbt_project_directory, check=True)
 
-    # Make manifest
-    subprocess.run(["dbt", "compile"], cwd=dbt_project_directory, check=True)
+RELATIVE_PATH_TO_MY_DBT_PROJECT = "../de-zoomcamp-2025/dbt_clickhouse_taxy_rides"
 
-    manifest_path = dbt_project_directory / "target" / "manifest.json"
-    persistent_manifest_path = Path("path/to/persistent/directory") / "manifest.json"
-    shutil.copy(manifest_path, persistent_manifest_path)
+dbt_project_directory = DbtProject(
+    project_dir=Path(__file__)
+    .parent
+    .joinpath(RELATIVE_PATH_TO_MY_DBT_PROJECT)
+    .resolve(),
+)
 
-    dbt_project.manifest_path = persistent_manifest_path
+# Initialize dbt project
+dbt_project = DbtProject(project_dir=dbt_project_directory)
+dbt_resource = DbtCliResource(project_dir=dbt_project)
 
-    # Initialize dbt project
-    dbt_project = DbtProject(project_dir=dbt_project_directory)
-    dbt_resource = DbtCliResource(project_dir=dbt_project)
-    
-    # Prepare dbt project
-    dbt_project.prepare_if_dev()
+# Prepare dbt project
+dbt_project.prepare_if_dev()
 
 # Asset definition remains the same
 @dbt_assets(manifest=dbt_project.manifest_path)
