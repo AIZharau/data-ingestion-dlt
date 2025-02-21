067f99e6e5deeae3fb43398746d8632d6c6ed86e
AIZharau <alieszhar@gmail.com>
2025-02-21 23:21:54 +0300

    fix .env

--- src/resources/s3.py
+++ src/resources/s3.py
@@ -2,7 +2,7 @@ from dagster import ConfigurableResource
 
 
 class S3Resource(ConfigurableResource):
-    aws_acces_key_id: str
-    aws_secret_acces_key: str
+    aws_access_key_id: str
+    aws_secret_access_key: str
     endpoint_url: str
     bucket_url: str
